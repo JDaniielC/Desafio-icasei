19789cc3db265b4c6ce68cc49d8d9fc273c3de68
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-01 23:59:14 -0300

    Feat: Add vite, configure vite scripts, vite config and begin to create drawer

---
+++ web_frontend/mf_videos/vite.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig(() => { 
+  return {
+    root: 'src',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    }
+  }
+})
